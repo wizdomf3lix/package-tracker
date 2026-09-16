af078a29cf6a5a010e41703134f88a2b75af2048
wizdomf3lix <wizdomf3lix@gmail.com>
2026-09-16 16:36:23 -0400

    Update Google Maps embed URL based on trackingId

--- src/App.tsx
+++ src/App.tsx
@@ -243,7 +243,7 @@ export default function App() {
                       referrerPolicy="no-referrer-when-downgrade"
                       src={
                         trackingId === 'B4V9Z1H6K3M8P2Q5W7Y0'
-                          ? "https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d11515104.711860918!2d9.69628783005427!3d45.20116609337537!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e6!4m5!1s0x47c609c3db87e4bb%3A0xb3a175ceffbd0a9f!2sNetherlands!3m2!1d52.132633!2d5.291265999999999!4m5!1s0x14b0155c964f2671%3A0x40d9dbd42a625f2a!2zVMO8cmtpeWU!3m2!1d38.963744999999996!2d35.243322!5e0!3m2!1sen!2sus!4v1789588735126!5m2!1sen!2sus"
+                          ? "https://www.google.com/maps/embed?pb=!1m52!1m12!1m3!1d11548523.907624193!2d9.708832648152562!3d45.03580838529067!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m37!3e0!4m5!1s0x47c609c3db87e4bb%3A0xb3a175ceffbd0a9f!2sNetherlands!3m2!1d52.132633!2d5.291265999999999!4m5!1s0x4741837bdf37e4c3%3A0xc4290c1e1010!2sHungary!3m2!1d47.162493999999995!2d19.503304099999998!4m5!1s0x479a721ec2b1be6b%3A0x75e85d6b8e91e55b!2sGermany!3m2!1d51.165690999999995!2d10.451526!4m5!1s0x47571ddff2898095%3A0x55e50ea3723865d!2sSerbia!3m2!1d44.016521!2d21.005858999999997!4m5!1s0x40a8fec1c85bf089%3A0xa01269bf4c10!2sBulgaria!3m2!1d42.733883!2d25.48583!4m5!1s0x14b0155c964f2671%3A0x40d9dbd42a625f2a!2zVMO8cmtpeWU!3m2!1d38.963744999999996!2d35.243322!5e0!3m2!1sen!2sus!4v1789590716813!5m2!1sen!2sus"
                           : `https://maps.google.com/maps?q=${encodeURIComponent(result.location)}&t=m&z=13&ie=UTF8&iwloc=&output=embed`
                       }
                     />
